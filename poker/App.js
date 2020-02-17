2902d02da90b5649a2ab584daeceb7a535de6cd0
mohammed mustafa <mohammed.alkhddar@gmail.com>
2020-02-17 00:56:48 -0600

    fixed cards

--- poker/App.js
+++ poker/App.js
@@ -3,10 +3,27 @@ const App = (props) => {
     let { cards } = props;
     console.log(props)
     return (
-        <span>cards</span>
+        <div>
+            <Card card={cards}/>
+    
+        </div>
     )
 }
 
 // CHALLENGE: Write a separate Card component for use in the App component
 // TIP: Use props to pass the suit and number to each Card component
-const Card = null;  
+const Card = (props) =>{
+    let cards=props.card
+console.log(cards)
+let card=cards.map(card=>{
+    return  <img width="80" key={card} src={'cards/' + card.value+card.suit+ '.png'}/>
+})
+
+return (
+    <div>
+{card}
+    </div>
+)
+
+}
+{/* <img width=80 src="cards/${card.value}${card.suit}.png"/> */}
